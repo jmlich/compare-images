7b0a528d5ca46e0f2d6f7ff79d58a932ed169279
Karel Koplík <karel.koplik@gmail.com>
2019-03-27 15:27:21 +0100

    Implemented distance calculation.

--- compare-images.py
+++ compare-images.py
@@ -5,40 +5,38 @@ import cv2
 import numpy as np
 import sys
 
-def threshold_slow(T, image):
-    # grab the image dimensions
-    h = image.shape[0]
-    w = image.shape[1]
-
-    mask = image >= T
-    image[mask] = 1
-    image[np.logical_not(mask)] = 0
-
-    return image
-
 if __name__ == "__main__":
 
     if (len(sys.argv) != 3) :
-        print ("usage: {0} in/mask001.png out/mask001.png".format(sys.argv[0]))
+        print( "usage: {0} image1.jpg image2.jpg".format( sys.argv[0] ) )
         sys.exit(1)
 
     inA_filename = sys.argv[1]
     inB_filename = sys.argv[2]
 
+    # jpg 8-bit 3 kanaly
+
     inA = cv2.imread(inA_filename)
     inB = cv2.imread(inB_filename)
 
-    # jpg 8-bit 3 kanaly
+    # prevest na [120, 90]
 
-    # prevest na [100, 100]
+    cols = 120
+    rows = 90
+
+    resizedA = cv2.resize( inA, ( cols, rows ) )
+    resizedB = cv2.resize( inB, ( cols, rows ) )
 
     # pro kazdy pixel spocitat absolutni hodnotu 
     # [ 0 ; 3 * 255 ] pro jeden pixel
-    # pro obrazek 100x100 [0 ; 7 650 000 ]
+    # pro obrazek 120, 90 [0 ; 8 262 000 ]
 
+    distance = 0
 
-#    mask_out = threshold_slow(100, mask_in_gray )
+    for y in range( rows ):
+        for x in range( cols ):
+            for i in range( 3 ):
+                distance = distance + abs( resizedA[ y, x, i ] - resizedB[ y, x, i ] )
 
-    distance=300
     print(distance)
 
